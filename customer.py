ae479d6e1b03901976bd627b803a57e8fff87d6e
Rochelle Awuor <awuorochelle75@gmail.com>
2025-05-18 11:11:47 +0300

    adds classes

---
+++ customer.py
@@ -0,0 +1,14 @@
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self._orders = []
+
+    def add_order(self, order):
+        self._orders.append(order)
+
+    def orders(self):
+        return self._orders
+
+    def coffees(self):
+        return list({order.coffee for order in self._orders})
