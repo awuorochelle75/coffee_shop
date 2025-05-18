7f575e71c4d871a864874968f53e677fcffba08f
Rochelle Awuor <awuorochelle75@gmail.com>
2025-05-18 11:37:10 +0300

    adds the validations for names

--- customer.py
+++ customer.py
@@ -4,6 +4,21 @@ class Customer:
         self.name = name
         self._orders = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+        
+
+
+    
+
     def add_order(self, order):
         self._orders.append(order)
 
